f99d5f6b463e3abca71a67c2547f14995c40e43c
asklootz <askl@uia.no>
2025-01-22 17:50:22 +0100

    Final push for Modul2

--- Modul2/Modul2/Program.cs
+++ Modul2/Modul2/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            //new Multiplication().Calculate(); //Running task 1
+            new Multiplication().Calculate(); //Running task 1
 
-            /*
+            
             Car myCar = new Car(); //Running task 2
             myCar.SetBrand("Mercedes");
             myCar.SetModel("S-Class");
@@ -14,7 +14,7 @@
             myCar.GetBrand();
             myCar.GetModel();
             myCar.GetYear();
-            */
+            
 
             Calculator myCalculator = new Calculator(); //Running task 3
             myCalculator.Calculate();
