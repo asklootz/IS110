233211129d260e326a6312532b7b02c1f7e44c8b
asklootz <askl@uia.no>
2025-01-27 13:31:53 +0100

    Added Module 3

---
+++ Modul3/Modul3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Modul3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student newStudent = new Student();
+
+            Console.Write("Enter name: ");
+            newStudent.name = Console.ReadLine();
+            Console.Write("Enter age: ");
+            newStudent.age = int.Parse(Console.ReadLine());
+            Console.Write("Enter average grade (1.0 to 6.0): ");
+            newStudent.averageGrade  = double.Parse(Console.ReadLine());
+
+            newStudent.WriteInfo();
+
+            if (newStudent.DidPass())
+            {
+                Console.WriteLine("Student passed");
+            }
+            else
+            {
+                Console.WriteLine("Student failed");
+            }
+
+            Console.Write("Enter years: ");
+            newStudent.years = int.Parse(Console.ReadLine());
+            Console.Write("Enter improvement: ");
+            newStudent.improvement = double.Parse(Console.ReadLine());
+
+            newStudent.ExpectedGradesAfterXYears(newStudent.improvement);
+            
+            if (newStudent.age >= 18 && newStudent.averageGrade >= 5.0)
+            {
+                Console.WriteLine($"{newStudent.name} is 18 years or older and has an average grade of 5.0 or higher");
+            }
+            else if (newStudent.age < 18 || newStudent.name[0] == 'S')
+            {
+                Console.WriteLine($"{newStudent.name} is younger than 18 or name starts with 'S'");
+            }
+        }
+    }
+}
---
+++ Modul3/Modul3/Student.cs
@@ -0,0 +1,40 @@
+using System.Threading.Tasks.Dataflow;
+
+class Student
+{
+    public Student()
+    {
+        // Constructor
+    }
+    public string name;
+    public int age;
+    public double averageGrade;
+    public int years;
+    public double improvement;
+    double newAverageGrade;
+
+    public void WriteInfo()
+    {
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Average grade: " + averageGrade);
+    }
+    public bool DidPass()
+    {
+        if (averageGrade >= 4.0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void ExpectedGradesAfterXYears(double improvement)
+    {
+        newAverageGrade = averageGrade * improvement + averageGrade;
+        Console.WriteLine($"Expected average grade after {years} years with improvement of {improvement} per year is: {newAverageGrade}");
+    }
+
+}
