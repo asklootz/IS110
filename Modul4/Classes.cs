ca61d8b4324d408708b76761355730d49d09cdd9
asklootz <askl@uia.no>
2025-02-10 11:15:41 +0100

    Added all tasks for this module
    Missing to update comments and readme.md

--- Modul4/Classes.cs
+++ Modul4/Classes.cs
@@ -1,4 +1,4 @@
-class Account
+class Account // Class for task 4.9
 {
     public string name { get; set; }
     private decimal balance;
@@ -28,7 +28,101 @@ class Account
     {
         if (depositAmount > 0.0m)
         {
-            Balance += depositAmount;
+            balance += depositAmount;
+            Console.WriteLine($"Depositing {depositAmount:C} to {name}'s account.");
         }
+    }
+
+    public void Withdraw(decimal withdrawAmount)
+    {
+        if (withdrawAmount < balance)
+        {
+            Balance -= withdrawAmount;
+            Console.WriteLine($"Withdrawing {withdrawAmount:C} from {name}'s account.");
+        }
+        else
+        {
+            Console.WriteLine("Withdrawal amount exceeded account balance! Cannot withdraw.");
+        }
+    }
+}
+
+
+class Student // Class for task 4.10
+{
+    public string id { get; set; }
+    public string name { get; set; }
+    public decimal mathGrade { get; set; }
+    public decimal englishGrade { get; set; }
+    public decimal scienceGrade { get; set; }
+
+    public Student(string studentId, string studentName, decimal math, decimal english, decimal science)
+    {
+        id = studentId;
+        name = studentName;
+        mathGrade = math;
+        englishGrade = english;
+        scienceGrade = science;
+    }
+
+    public decimal GetAggregate()
+    {
+        return (mathGrade + englishGrade + scienceGrade);
+    }
+
+    public decimal GetPercentage()
+    {
+        return GetAggregate() / 300 * 100;
+    }
+}
+
+
+class Asset // Class for task 4.11
+{
+    string name { get; set; }
+    decimal value { get; set; }
+    decimal depreciationRate { get; set; }
+
+    public Asset(string assetName, decimal assetValue, decimal assetDepreciationRate)
+    {
+        name = assetName;
+        value = assetValue;
+        depreciationRate = assetDepreciationRate;
+    }
+
+    public void Depreciate()
+    {
+        value -= value * depreciationRate;
+        Console.WriteLine($"\nDepreciating {name} by {depreciationRate:P}. New value is {value:C}");
+    }
+
+    public void IncreasedValue(decimal increase)
+    {
+        value += increase * value;
+        Console.WriteLine($"Increasing {name} by {increase:P}. New value is {value:C}");
+        value -= value * depreciationRate;
+        Console.WriteLine($"Depreciating {name} by {depreciationRate:P}. New value is {value:C}");
+    }
+}
+
+
+class Coaching  // Class for task 4.12
+{
+    string coachingType { get; set; }
+    int amountPlayers { get; set; }
+    string classTimings { get; set; }
+    decimal charges { get; set; }
+
+    public Coaching(string type, int players, string timings, decimal newCharges)
+    {
+        coachingType = type;
+        amountPlayers = players;
+        classTimings = timings;
+        charges = newCharges;
+    }
+
+    public void Coachingresults()
+    {
+        Console.WriteLine($"Coaching type: {coachingType}\t Number of players: {amountPlayers}\t Timings: {classTimings}\t Charges: {charges}");
     }
 }
--- Modul4/Program.cs
+++ Modul4/Program.cs
@@ -10,21 +10,42 @@
             Console.WriteLine($"{account1.name}'s balance: {account1.Balance:C}");
             Console.WriteLine($"{account2.name}'s balance: {account2.Balance:C}");
 
-            Console.Write("\nEnter deposit amount for account1: ");
+            Console.Write($"\nEnter deposit amount for {account1.name}: ");
             decimal depositAmount = decimal.Parse(Console.ReadLine());
-            Console.WriteLine($"adding {depositAmount:C} to account1 balance");
             account1.Deposit(depositAmount);
 
-            Console.WriteLine($"{account1.name}'s balance: {account1.Balance:C}");
-            Console.WriteLine($"{account2.name}'s balance: {account2.Balance:C}");
-
-            Console.Write("\nEnter deposit amount for account2: ");
+            Console.Write($"\nEnter deposit amount for {account2.name}: ");
             depositAmount = decimal.Parse(Console.ReadLine());
-            Console.WriteLine($"adding {depositAmount:C} to account2 balance");
             account2.Deposit(depositAmount);
 
             Console.WriteLine($"\n{account1.name}'s balance: {account1.Balance:C}");
             Console.WriteLine($"{account2.name}'s balance: {account2.Balance:C}");
+
+            Console.Write("\nEnter withdrawal amount for account1: ");
+            decimal withdrawAmount = decimal.Parse(Console.ReadLine());
+            account1.Withdraw(withdrawAmount);
+
+            Console.WriteLine("\nEnter withdrawal amount for account2: ");
+            withdrawAmount = decimal.Parse(Console.ReadLine());
+            account2.Withdraw(withdrawAmount);
+
+            Console.WriteLine($"\n{account1.name}'s balance: {account1.Balance:C}");
+            Console.WriteLine($"{account2.name}'s balance: {account2.Balance:C}");
+
+
+            Student newStudent = new Student("12345", "John Doe", 90, 21, 50 );
+            Console.WriteLine($"Student {newStudent.name} has an aggregate of {newStudent.GetAggregate()} and a percentage of {newStudent.GetPercentage()}%");
+
+            Asset laptop = new Asset("Laptop", 1000, 0.01m);
+            laptop.Depreciate();
+            laptop.IncreasedValue(0.05m);
+            Asset phone = new Asset("Phone", 500, 0.02m);
+            phone.Depreciate();
+            phone.IncreasedValue(0.1m);
+
+            Coaching newCoaching = new Coaching("Football", 20, "10:00", 10.5m);
+            newCoaching.Coachingresults();
+
         }
     }
 }
